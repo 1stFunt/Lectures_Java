816132cb9c552f8721ca985a2b2fb25d8a76f939
Funt <funt_88@mail.ru>
2023-03-30 01:03:57 +0300

    StringBuilder, Files

--- Seminar2.java
+++ Seminar2.java
@@ -4,6 +4,7 @@ public class Seminar2 {
         // task1(); // Написать метод, который вернёт строку длины N, которая состояит из чередующихся символов s1 и s2
         // task2_1(); // Написать метод, который сжимает строку
         task2_2(); // Второй способ решения второй задачи
+        // task5("C:\\Users\\Funt\\Desktop\\Java"); // Должно работать в IDEA
     }
 
     private static void test() {
@@ -76,4 +77,24 @@ public class Seminar2 {
 
         System.out.printf("%s -> %s%n", longString, shortStringSB);
     }
+
+    // private static void task5(String pathDir) {
+    //     File file = new File(pathDir);
+    //     if (!file.isDirectory()) {
+    //         return;
+    //     }
+
+    //     String[] dirListNames = file.list();
+
+    //     StringBuilder sb = new StringBuilder();
+    //     for (String fileName : dirListNames) {
+    //         sb.append(fileName).append(System.lineSeparator());
+    //     }
+        
+    //     try (PrintWriter pw = new PrintWriter("src/main/resources/files/output.txt")) { // взять относительный путь 
+    //         pw.print(sb);                                                               // вновь созданного файла
+    //     } catch (FileNotFoundException e) {
+    //         throw new RuntimeException(e);
+    //     }
+    // }
 }
